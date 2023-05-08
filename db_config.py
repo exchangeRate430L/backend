6b1f6e2b5398787c8b20639bf488b939f4149196
Karim Safar <kss22@mail.aub.edu>
2023-05-08 15:28:13 +0300

    fixed db_config

--- db_config.py
+++ db_config.py
@@ -1,2 +1,2 @@
-DB_CONFIG = 'mysql+pymysql://root:1732kanashawaK@localhost:3306/exchange'
-# DB_CONFIG = 'mysql://b250d2889d8486:2e8ade03@us-cdbr-east-06.cleardb.net/heroku_64859324a4212cb?reconnect=true'
+# DB_CONFIG = 'mysql+pymysql://root:1732kanashawaK@localhost:3306/exchange'
+DB_CONFIG = 'mysql://b250d2889d8486:2e8ade03@us-cdbr-east-06.cleardb.net/heroku_64859324a4212cb?reconnect=true'
